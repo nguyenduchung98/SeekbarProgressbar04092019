ffe6eb310dad8be6d3663fa0b078c879acae75f1
Nguyen Duc Hung <hungrom98x@gmail.com>
2019-10-23 20:38:37 +0700

    seekbar

--- app/src/main/java/nguyenduchung/ndh/seekbarprogressbar04092019/MainActivity.java
+++ app/src/main/java/nguyenduchung/ndh/seekbarprogressbar04092019/MainActivity.java
@@ -3,12 +3,37 @@ package nguyenduchung.ndh.seekbarprogressbar04092019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+    SeekBar sk,sk1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sk=findViewById(R.id.seekbar);
+        sk1=findViewById(R.id.seekbar1);
+
+        sk.setIndeterminate(true);//tự động chạy của seekbar
+        sk1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if(progress > sk.getProgress()){
+                    sk1.setProgress(sk.getProgress());
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
---
+++ f
@@ -0,0 +1,17 @@
+[33mcommit fd2b87e5b9d3c45df2c4fb40cb3e3621d0d71134[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/master[m[33m)[m
+Author: Nguyen Duc Hung <hungrom98x@gmail.com>
+Date:   Wed Oct 23 20:09:31 2019 +0700
+
+    B
+
+[33mcommit 2169a7677ef475973e08a9764ecbd6e1f33394b4[m
+Author: Nguyen Duc Hung <hungrom98x@gmail.com>
+Date:   Wed Oct 23 20:08:41 2019 +0700
+
+    A
+
+[33mcommit 78a42719a2eea0366ce52eff597e9aa3ebca2c85[m
+Author: Nguyen Duc Hung <hungrom98x@gmail.com>
+Date:   Wed Oct 23 20:07:22 2019 +0700
+
+    first commit
